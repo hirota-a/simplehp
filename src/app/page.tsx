49dea85d75e3fb0985562b9703a47b65c401e608
hirota <aoaoworks766@gmail.com>
2025-07-18 23:01:57 +0900

    test5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://use.typekit.net/vmq0vek.css"></link>
+        <link rel="stylesheet" href="https://use.typekit.net/vmq0vek.css"/>
       </Head>
       <main>
         <div>
-          <h1 className="text-6xl font-handwriting mb-4 text-center text-cyan-500 font-handwriting">
+          <h1 className="text-6xl font-handwriting mb-4 text-center text-cyan-500">
             HIROTA
           </h1>
           <p className="text-lg float-right pr-4">
